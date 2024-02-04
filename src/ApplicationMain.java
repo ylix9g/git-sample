e65459f968093c72dc5fbba6f8579410829eb130
Igor Polichshuk <ylix9g@gmail.com>
2024-02-04 13:10:43 +0600

    Added cycle into main()

--- src/ApplicationMain.java
+++ src/ApplicationMain.java
@@ -2,5 +2,9 @@ public class ApplicationMain {
 
     public static void main(String[] args) {
         System.out.println("--- Application ---");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
     }
 }
